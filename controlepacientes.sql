e6cdb7ca75b8a40736699ccc0dff38e3b51ae94b
Sindy Aguirre <sindy_antunes@hotmail.com>
2019-06-03 23:46:33 -0300

    alteracoes nas tabelas do banco

--- controlepacientes.sql
+++ controlepacientes.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: 03-Jun-2019 às 05:57
+-- Generation Time: 04-Jun-2019 às 02:44
 -- Versão do servidor: 5.7.23
 -- versão do PHP: 7.2.10
 
@@ -38,15 +38,17 @@ CREATE TABLE IF NOT EXISTS `pessoa` (
   `idtipoPessoa` int(11) DEFAULT NULL,
   `dataCadastro` datetime DEFAULT NULL,
   PRIMARY KEY (`idpessoa`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `pessoa`
 --
 
 INSERT INTO `pessoa` (`idpessoa`, `nome`, `dataNascimento`, `sexo`, `cpf`, `idtipoPessoa`, `dataCadastro`) VALUES
-(10, 'Sm/Do28gRmVpasOz', '2010-12-11', 2, '09809809', 1, '2019-06-02 23:37:43'),
-(12, 'Joaquina ação teste', '2010-01-11', 1, '12321', 1, '2019-06-03 00:50:34');
+(12, 'Joaquina alterado', '2010-01-11', 1, '12365564654', 1, '2019-06-03 00:50:34'),
+(13, 'joão Rosa alterado', '1987-08-11', 2, '9898988080', 1, '2019-06-03 06:18:14'),
+(14, 'Maria Rosa Mada', '1998-02-10', 1, '98989898998', 1, '2019-06-04 00:28:42'),
+(16, 'Joaquina', '1999-03-11', 1, '23434324234', 1, '2019-06-04 01:29:21');
 
 -- --------------------------------------------------------
 
@@ -84,14 +86,15 @@ CREATE TABLE IF NOT EXISTS `usuario` (
   `tipoUsuario` int(11) NOT NULL,
   `usuario` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`idusuario`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `usuario`
 --
 
 INSERT INTO `usuario` (`idusuario`, `nome`, `email`, `senha`, `dataCadastro`, `tipoUsuario`, `usuario`) VALUES
-(4, 'user teste', 'teste@teste.com', '2ca72350ffc67eaec75f79ac14ec7358', '2019-06-02 18:05:00', 1, 'hammer');
+(4, 'user teste', 'teste@teste.com', '2ca72350ffc67eaec75f79ac14ec7358', '2019-06-02 18:05:00', 1, 'hammer'),
+(5, 'Maria Rosa', 'maria@hotmail.com', '202cb962ac59075b964b07152d234b70', '2019-06-04 00:04:43', 1, NULL);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
